97df12279464cb327b73d2e9d497d10e6a1d162f
atomiccccc <ankit.kaswebtech@gmail.com>
2024-05-02 18:43:55 +0530

    update webhooks & send storefront api key

--- web/gdpr.js
+++ web/gdpr.js
@@ -1,46 +1,71 @@
 import { DeliveryMethod } from "@shopify/shopify-api";
+import crypto from 'crypto';
+import 'dotenv/config';
+
+async function verifyShopifyWebhooks(req, res, next) {
+
+  const hmac = req.query.hmac;
+
+  if (!hmac) {
+    return res.status(401).send("Webhook must originate from Shopify!");
+  }
+  const genHash = crypto
+    .createHmac("sha256", process.env.SHOPIFY_API_SECRET)
+    .update(JSON.stringify(req.body))
+    .digest("base64");
+
+  if (genHash !== hmac) {
+    return res.status(401).send("Couldn't verify incoming Webhook request!");
+  }
+
+next();
+
+}
 
 export default {
-  CUSTOMERS_DATA_REQUEST: {
-      deliveryMethod: DeliveryMethod.Http,
-      callbackUrl: "/api/webhooks",
-      callback: async (topic, shop, body, webhookId) => {
-        const payload = JSON.parse(body);
-        console.log("Received customer data request webhook:", payload);
-      },
-    },
-  CUSTOMERS_REDACT: {
-      deliveryMethod: DeliveryMethod.Http,
-      callbackUrl: "/api/webhooks",
-      callback: async (topic, shop, body, webhookId) => {
-        const payload = JSON.parse(body);
-        console.log("Received customer data redact webhook:", payload);
-      },
-    },
-  SHOP_REDACT: {
-      deliveryMethod: DeliveryMethod.Http,
-      callbackUrl: "/api/webhooks",
-      callback: async (topic, shop, body, webhookId) => {
-        const payload = JSON.parse(body);
-        console.log("Received shop data redact webhook:", payload);
-      },
-    },
   APP_UNINSTALLED: {
     deliveryMethod: DeliveryMethod.Http,
     callbackUrl: "/api/webhooks/app-uninstall",
     callback: async (topic, shop) => {
       // Make HTTP request to the specified API endpoint
       try {
-          const response = await fetch(`https://tellos-xyz.link/shopify/uninstall?shop=${shop}`);
-          
+          const response = await fetch(process.env.TELLOS_API_BASE_URL+`shopify/uninstall?shop=${shop}`);
           if (!response.ok) {
             throw new Error('Failed to hit the API endpoint');
-          }else{
-            return res.sendStatus(200);
           }
         } catch (error) {
           console.error("Error hitting API endpoint:", error);
         }
+    },
+  },
+  CUSTOMERS_DATA_REQUEST: {
+    deliveryMethod: DeliveryMethod.Http,
+    callbackUrl: "/api/webhooks",
+    verifyShopifyWebhooks,
+    callback: async (topic, shop, body, webhookId) => {
+      const payload = JSON.parse(body);
+      console.log("Received customer data request webhook:", payload);
+      res.sendStatus(200);
+    },
+  },
+  CUSTOMERS_REDACT: {
+    deliveryMethod: DeliveryMethod.Http,
+    callbackUrl: "/api/webhooks",
+    verifyShopifyWebhooks,
+    callback: async (topic, shop, body, webhookId) => {
+      const payload = JSON.parse(body);
+      console.log("Received customer data redact webhook:", payload);
+      res.sendStatus(200);
+    },
+  },
+  SHOP_REDACT: {
+    deliveryMethod: DeliveryMethod.Http,
+    callbackUrl: "/api/webhooks",
+    verifyShopifyWebhooks,
+    callback: async (topic, shop, body, webhookId) => {
+      const payload = JSON.parse(body);
+      console.log("Received shop data redact webhook:", payload);
+      res.sendStatus(200);
     },
   },
 };
